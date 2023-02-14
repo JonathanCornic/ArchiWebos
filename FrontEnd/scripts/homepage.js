bf2b71bfba841df41eb5915dd27eb29dcd02f073
cornic jonathan <cornic_jonathan@orange.fr>
2023-02-14 03:12:33 +0100

    refactoring html css et js

--- FrontEnd/scripts/homepage.js
+++ FrontEnd/scripts/homepage.js
@@ -6,54 +6,6 @@ console.log(projets);
 // balise de ratachement
 const portfolio = document.querySelector("#portfolio");
 
-// creation du conteneur filtre
-const conteneurFiltre = document.createElement("div");
-conteneurFiltre.className = "conteneurFiltre";
-
-// creation des boutons de filtre
-const filtreTous = document.createElement("button");
-filtreTous.className = "btn btnTous"
-filtreTous.innerHTML = "Tous";
-const filtreObjets = document.createElement("button");
-filtreObjets.className = "btn btnObjets"
-filtreObjets.innerHTML = "Objets";
-const filtreAppartements = document.createElement("button");
-filtreAppartements.className = "btn btnAppartements"
-filtreAppartements.innerHTML = "Appartements ";
-const filtreHotelsEtRestaurants = document.createElement("button");
-filtreHotelsEtRestaurants.className = "btn btnHotelEtRestaurant"
-filtreHotelsEtRestaurants.innerHTML = "Hôtels & restaurants";
-
-// attachement parent enfant 
-portfolio.appendChild(conteneurFiltre);
-conteneurFiltre.appendChild(filtreTous);
-conteneurFiltre.appendChild(filtreObjets);
-conteneurFiltre.appendChild(filtreAppartements);
-conteneurFiltre.appendChild(filtreHotelsEtRestaurants);
-
-// css du conteneur de filtre
-conteneurFiltre.style.width="65%";
-conteneurFiltre.style.margin="40px auto";
-conteneurFiltre.style.display="flex";
-conteneurFiltre.style.justifyContent="space-between";
-
-// fonction pour le css des boutons
-function cssBoutons(){
-    let boutons = document.querySelectorAll(".btn");
-    for(let i = 0; i < boutons.length; i++) {
-
-        boutons[i].style.color="#1D6154";
-        boutons[i].style.backgroundColor="#FFFEF8";
-        boutons[i].style.border="2px solid";
-        boutons[i].style.borderColor="#1D6154";
-        boutons[i].style.fontSize="20px";
-        boutons[i].style.fontWeight="700";
-        boutons[i].style.borderRadius="35px";
-        boutons[i].style.padding="10px 30px";
-    }
-}
-cssBoutons();
-
 // creation de la sectionGallery
 const sectionGallery = document.createElement("div");
 sectionGallery.className = "gallery";
@@ -67,16 +19,25 @@ function genererProjets(projets){
         imageProjet.src = element.imageUrl;
         const titreProjet = document.createElement("figcaption");
         titreProjet.innerHTML = element.title;
-
+        
         portfolio.appendChild(sectionGallery);
         sectionGallery.appendChild(carteProjet);
         carteProjet.appendChild(imageProjet);
         carteProjet.appendChild(titreProjet);
         
     });
-
+    
 }
 genererProjets(projets);
+
+// selection conteneur filtre
+const conteneurFiltre = document.querySelector(".conteneurBtn").style.display="flex";
+
+// selection des boutons de filtre
+const filtreTous = document.querySelector(".btnTous");
+const filtreObjets = document.querySelector(".btnObjets");
+const filtreAppartements = document.querySelector(".btnAppartements");
+const filtreHotelsEtRestaurants = document.querySelector(".btnHotelsEtRestaurants");
 
 // fonction pour le visuel du bouton actif
 function couleurFiltre(bouton, click) {
@@ -125,24 +86,27 @@ filtreHotelsEtRestaurants.addEventListener("click", function(){
 
 
 
-const loginLien = document.querySelector("#login");
-loginLien.addEventListener("click", () => {
-    window.location.href = 'login.html';
-});
+
 
 // fonction pour cacher les bouton
 function montrerCacherBouttons(){
-    const display = document.querySelector(".conteneurFiltre").style.display;
+    const display = document.querySelector(".conteneurBtn").style.display;
     if(display === "flex"){
-        document.querySelector(".conteneurFiltre").style.display="none";
-        document.querySelector("#portfolio h2").style.marginBottom="134px";
+        document.querySelector(".conteneurBtn").style.display="none";
+        document.querySelector("#portfolio h2").style.marginBottom="125px";
 
     }else if(display === "none"){
-        document.querySelector(".conteneurFiltre").style.display="flex"
+        document.querySelector(".conteneurBtn").style.display="flex";
         document.querySelector("#portfolio h2").style.marginBottom="0";
         
     }
 };
+
+
+
+
+
+
 
 
 
